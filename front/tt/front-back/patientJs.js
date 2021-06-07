baba841e985c2afae5d9d1fb214b7f264a79f3ed
MelinaLushaku <63053349+MelinaLushaku@users.noreply.github.com>
2021-06-08 01:56:41 +0200

    ConversationFunction

--- front/tt/front-back/patientJs.js
+++ front/tt/front-back/patientJs.js
@@ -1,20 +1,24 @@
 function getDocCards() {
+    
     $.ajax({
         type: "GET",
         url: "http://localhost:8080/api/systemManagement/admin/getAllDoctors",
         contentType: "application/json; charset=utf-8",
         dataType: "json",
         success: function(result) {
+            
             var y = result.data;
             if (y != null) {
                 $.each(y, function(i, item) {
                    $("#hapsiraKryesore").append('<div class="col-md-3" style="float:left"><div class="card mb-2"><img class="card-img-top profile" src="assets/img/femdoc.jpg" alt="Card image cap"><div class="card-body"><h4 class="card-title">' 
-                   +item.doctorName+' '+item.doctorSurname+'</h4><p class="card-text">'+item.departmentN+': '+item.doctorSpecialization+'</p><button id="butonisBisedes" class="btn btn-outline-primary "   type="submit" data-toggle="modal" data-target="#chatModal">Chat</button><!-- Chat --><div class="modal fade" id="chatModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true"><div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header"><h5 class="modal-title" id="exampleModalLabel">Chat</h5><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span>'
-                   +'</button></div><div class="modal-body"><form onsubmit="return false;"><div class="chat-messages"><div class="message-box-holder"><div class="message-box">Hello</div></div>'
-                   +'<div class="message-box-holder"><div class="message-box message-partner" id="fjalet"></div></div>'
+                   +item.doctorName+' '+item.doctorSurname+'</h4><p class="card-text">'+item.departmentN+': '+item.doctorSpecialization+'</p><p>Chat: </p><button class="bvepVallai" type="button" data-toggle="modal" data-target="#chatModal" id="butonisBisedes">'+item.personalNumber+'</button><!-- Chat --><div class="modal fade" id="chatModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true"><div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header"><h5 class="modal-title" id="exampleModalLabel">Chat</h5><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span>'
+                   +'</button></div><div class="modal-body"><form onsubmit="return false;"><div class="chat-messages"><div id="sendFjalet" class="message-box-holder"></div>'
+                   +'<div id="fjalet" class="message-box-holder"></div>'
                    +'</div><input type="text" class="form-control" id="recipient-name" placeholder="Type a message..."><div class="modal-footer"><button class="btn btn-primary">Delete Conversation</button><button class="btn btn-primary">Send</button></div></form></div>'
                    +'</div></div></div></div></div></div>');
-                   document.getElementById("butonisBisedes").val = item.personalNumber;
+                 
+                 
+                   
                    //var vveq = document.getElementsByClassName("btn btn-outline-primary").value;
                    //console.log(vveq);
                  });
@@ -34,32 +38,43 @@ function faqjaEPersonalizuar(){
 
  document.getElementById("nrPersonal").innerHTML = "PatientNumber: "+ nrP;
 
+
+
 }
 
-
-
-$("#butonisBisedes").click(function(event) {
+$(document).on('click', '.bvepVallai', function(event) {
+//not finished!!!!
+$("#fjalet").empty();
+$("#sendFjalet").empty();
+    
     var patNumber = localStorage.getItem("persoanlPat");
-    var docNumber = document.getElementById("butonisBisedes").value;
+    var docNumber = $(this).text();
+    
+   
     $.ajax({
         url: "http://localhost:8000/api/conversationManagement/allConversation/"+docNumber+"/"+patNumber,
         type: 'GET',
         contentType: "application/json; charset=utf-8",
         dataType: "json",
         success: function(res) {
+           
                    var e = res.errori;
-                   var m
+                  
                    // localStorage.setItem('admin')
                    if(e == null){
                        var y = res.data;
                     $.each(y, function(i, item) {
-                        $("#fjalet").append('<div class="message-sender">'+res.data.conversationDes+'</div>');
+                        if(item.role == 1){
+                            $("#sendFjalet").append('You:<div class="message-box">'+item.conversationDes+'</div>');
+                        }else{
+                        $("#fjalet").append('<div class="message-sender">Doctor:<div class="message-box message-partner" >'+item.conversationDes+'</div></div>');
                         console.log(res.data.conversationDes);
+                        }
     
                       });
                       
                    }else{
-                    $("#fjalet").append(e);
+                    $("#sendFjalet").append(e);
                    }
                 }
     ,
@@ -70,7 +85,42 @@ $("#butonisBisedes").click(function(event) {
     })
 });
 
+$("#shfaqBisedat").click(function(event) {
+    var patNumber = localStorage.getItem("persoanlPat");
+    
+    $.ajax({
+        url: "http://localhost:8000/api/conversationManagement/conversationListPatient/"+patNumber,
+        type: 'GET',
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function(res) {
+                   
+                   var e = res.errori;
+                   var m
+                   // localStorage.setItem('admin')
+                   if(e == null){
+                       var y = res.data;
+                       
 
+                    $.each(y, function(i, item) {
+                        $("#convList").append('<li><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/chat_avatar_02.jpg" alt=""><div><h2>'+item.doctorName+' '+item.doctorSurname+'</h2></div></li>');
+                        console.log(res.data.conversationDes);
+    
+                      });
+                      
+                   }else{
+                    $("#convList").append(e);
+                   }
+                }
+    ,
+        error: function(error) {
+            console.log(error);
+               
+        }
+    })
+});
+
+//'<li><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/chat_avatar_02.jpg" alt=""><div><h2>'+Prénom Nom+'</h2></div></li>'
 
 
 
